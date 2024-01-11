1f30a4192657496bdbc25b92c840118534b47486
Saurav <saurav6295@yahoo.com>
2024-01-12 01:42:29 +0530

    first commit

---
+++ SpecFlowCalculator/Calculator.cs
@@ -0,0 +1,14 @@
+﻿namespace SpecFlowCalculator
+{
+    public class Calculator
+    {
+        public int FirstNumber{get;set;}
+        public int SecondNumber { get;set;}
+        //about get set : https://www.w3schools.com/cs/cs_properties.php
+        public int Add()
+        { 
+            return FirstNumber+SecondNumber; 
+        }
+
+    }
+}
